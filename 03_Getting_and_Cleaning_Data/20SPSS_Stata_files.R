e8a34a990e68e01c4c3a35794dd1efd6f1a9b3de
Venkatesh <40434922+VenkiPhy6@users.noreply.github.com>
2022-09-13 11:50:23 -0500

    SPSS Stata imports
    Figured out how to import SPSS and Stata files

--- 03_Getting_and_Cleaning_Data/20SPSS_Stata_files.R
+++ 03_Getting_and_Cleaning_Data/20SPSS_Stata_files.R
@@ -1,3 +1,32 @@
-#See https://github.com/VenkiPhy6/courses/blob/master/03_GettingData/02_05_readingFromOtherSources/index.md
+#Also see https://github.com/VenkiPhy6/courses/blob/master/03_GettingData/02_05_readingFromOtherSources/index.md
 library(foreign)
 
+#-----------------------------
+#STATA files
+
+#Downloading-----
+if(!file.exists('data')){
+  dir.create('data')
+}
+fileUrl <- "http://cameron.econ.ucdavis.edu/stata/carsdata.dta"
+download.file(fileUrl, destfile = './data/carsdata.dta', method = 'curl')
+
+carsdata <- read.dta('./data/carsdata.dta')
+tail(carsdata)
+
+#-----------------------------
+#SPSS files
+#My data is available to download in a zip file. So I am using to opportunity to
+#learn how to unzip a file in R!
+
+#Downloading and unzipping-----
+if(!file.exists('data')){
+  dir.create('data')
+}
+fileUrl <- "http://spss.allenandunwin.com.s3-website-ap-southeast-2.amazonaws.com/Files/survey.zip"
+download.file(fileUrl, destfile = './data/survey.zip', method = 'curl')
+#See https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/unzip for more on unzip
+unzip(zipfile = './data/survey.zip', exdir = './data') #You should not have a trailing '/' in exdir!
+
+survey <- read.spss('./data/survey.sav', to.data.frame = T)
+survey
